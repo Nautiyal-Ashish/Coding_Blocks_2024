eeaf42bf031dba4340c8dbf0ebcc4ff932544fe8
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-05-08 12:58:27 +0530

    Updated Prototype

--- Lecture 17 - JS/Prototypes/app.js
+++ Lecture 17 - JS/Prototypes/app.js
@@ -34,12 +34,47 @@
 // What are prototypes?
 // -> fallback source
 
-let person = {
-    naam: 10,
-    petDialogue: function () {
-      console.log("chapri chai wala");
-    },
-  };
+// let person = {
+//   naam: 10,
+//   petDialogue: function () {
+//     console.log("aaj kal ke nalle berozgaar cbhappri");
+//   },
+// };
 
-//  -------------------------
+// console.log(person.naam); //10
+// console.log(person.kaam); //undefined
+// person.hello(); //error
+// person.string(); //error
+// // ------
+// console.log(person.toString()); //[object object]
 
+// console.log(person); // will run perfectly
+
+// ------------
+
+// let person = {
+//   naam: 10,
+//   petDialogue: function () {
+//     console.log("aaj kal ke nalle berozgaar cbhappri");
+//   },
+// };
+// let peeps = {
+//   naam: 100,
+//   petDialogue: function () {
+//     console.log("hehehe aj kal ke nalle berozgaar cbhappri");
+//   },
+// };
+
+// // dender proto -> pitaji value batane ke liye
+// console.log(person); // will run the person function
+// console.log(peeps); // will run the peeps function
+// console.log(person.__proto__);
+// console.log(peeps.__proto__);
+
+// ------------
+
+let arr = [10, 20, 40, 50];
+
+// -------
+
+let str = "samarth";
