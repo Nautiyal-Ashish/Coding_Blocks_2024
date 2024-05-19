edffb40a395988130c96c6a7e37c061dd922bfe3
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-05-19 17:28:43 +0530

    Lecture 25 -  JS

---
+++ Lecture 25 - JS/canvas/app.js
@@ -0,0 +1,28 @@
+let canvas = document.querySelector("canvas"); //board
+let ctx = canvas.getContext("2d"); //brush
+
+// x , y, w , h
+// ctx.fillStyle = "red";
+// ctx.fillRect(100, 100, 150, 150);
+
+// ctx.strokeStyle = "blue";
+// ctx.strokeRect(100, 100, 150, 150);
+
+// draw paths
+// ctx.beginPath();
+
+// ctx.moveTo(100, 100);
+// ctx.lineTo(150, 150);
+// ctx.lineTo(50, 150);
+// ctx.lineTo(50, 50);
+// ctx.lineTo(150, 50);
+// // ctx.stroke();
+// ctx.fill();
+
+// ctx.closePath();
+
+// ----------
+ctx.font = "20px cursive";
+ctx.fillStyle = "green";
+ctx.fillText("Ashish", 10, 60);
+
