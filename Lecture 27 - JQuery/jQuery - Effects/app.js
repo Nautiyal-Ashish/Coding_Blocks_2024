90872e227874750fe0da8d0a94c542e8116950d5
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-05-26 18:59:44 +0530

    Lecture 26 and 27 - Snake Game

--- Lecture 27 - JQuery/jQuery - Effects/app.js
+++
@@ -1,9 +0,0 @@
-$("#fadein").click(function () {
-  $("#container").fadeIn();
-});
-$("#fadeout").click(function () {
-  $("#container").fadeOut();
-});
-$("#fadetoggle").click(function () {
-  $("#container").fadeToggle();
-});
