ffa36c375ef33a0eb38889519d21877967c18568
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-08-25 20:47:10 +0530

    Lecture 50 - Web Sockets with Chatting App

---
+++ Lecture 50 - Web Sockets/socket-demo/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);//app is request listener
+const path = require('path');
+const socketio = require('socket.io');
+const io = socketio(server);
+
+
+let users = {};
+
+io.on('connection', (socket) => {
+  console.log("connection established");
+  // console.log(socket)
+  socket.on('send-msg', (data) => {
+    console.log(data, "lol")
+    io.emit('receive-msg', { msg: data.msg, username: users[socket.id] })
+  })
+  socket.on('login', (data) => {
+    users[socket.id] = data.username;
+  })
+});
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+
+let PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Sever Connected at port : ${PORT}`);
+})
+
+
